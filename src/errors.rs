e89c01a84d99f6e56b43079e24a861e832b139d5
Nikolay Kim <fafhrd91@gmail.com>
2018-12-19 14:27:22 -0800

    make errors clonable

--- src/errors.rs
+++ src/errors.rs
@@ -3,7 +3,7 @@ use uuid;
 use crate::protocol::ProtocolId;
 use crate::types::Descriptor;
 
-#[derive(Debug, Display, From)]
+#[derive(Debug, Display, From, Clone)]
 pub enum AmqpParseError {
     #[display(fmt = "More data required during frame parsing: '{:?}'", "_0")]
     Incomplete(Option<usize>),
@@ -28,7 +28,24 @@ pub enum AmqpCodecError {
     ParseError(AmqpParseError),
     #[display(fmt = "bytes left unparsed at the frame trail")]
     UnparsedBytesLeft,
-    Io(std::io::Error),
+    #[display(fmt = "Io error: {:?}", _0)]
+    Io(Option<std::io::Error>),
+}
+
+impl Clone for AmqpCodecError {
+    fn clone(&self) -> AmqpCodecError {
+        match self {
+            AmqpCodecError::ParseError(err) => AmqpCodecError::ParseError(err.clone()),
+            AmqpCodecError::UnparsedBytesLeft => AmqpCodecError::UnparsedBytesLeft,
+            AmqpCodecError::Io(_) => AmqpCodecError::Io(None),
+        }
+    }
+}
+
+impl From<std::io::Error> for AmqpCodecError {
+    fn from(err: std::io::Error) -> AmqpCodecError {
+        AmqpCodecError::Io(Some(err))
+    }
 }
 
 #[derive(Debug, Display, From)]
