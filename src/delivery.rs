548627de0245c39750eaad328295b1c66d61110d
Nikolay Kim <fafhrd91@gmail.com>
2024-04-10 00:10:25 +0500

    wip

--- src/delivery.rs
+++ src/delivery.rs
@@ -134,6 +134,10 @@ impl Delivery {
     }
 
     pub async fn wait(&self) -> Result<Option<DeliveryState>, AmqpProtocolError> {
+        if self.0.flags.get().contains(Flags::LOCAL_SETTLED) {
+            return Ok(None);
+        }
+
         let rx = if let Some(inner) = self
             .session
             .inner
