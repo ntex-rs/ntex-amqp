4b62a8f426d3683e65a93e92394c71ea61bfd7d4
Nikolay Kim <fafhrd91@gmail.com>
2026-01-16 12:52:48 +0100

    Fix link credit handling

--- codec/src/codec/mod.rs
+++ codec/src/codec/mod.rs
@@ -150,7 +150,7 @@ mod tests {
         let mut buf = BytesMut::new();
         buf.reserve(frame.encoded_size());
         frame.encode(&mut buf);
-        let _ = buf.split_to(4);
+        buf.advance_to(4);
         assert_eq!(data2, buf.freeze());
 
         Ok(())
@@ -166,7 +166,7 @@ mod tests {
         let mut buf = BytesMut::new();
         buf.reserve(frame.encoded_size());
         frame.encode(&mut buf);
-        let _ = buf.split_to(4);
+        buf.split_to(4);
         assert_eq!(data, buf.freeze());
 
         Ok(())
